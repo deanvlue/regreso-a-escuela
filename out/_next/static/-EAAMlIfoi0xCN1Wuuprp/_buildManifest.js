e43bf63e38d91dbfa853cd2e9ee622c4f72e265f
Carlos <munoz.josecarlos@gmail.com>
2021-06-14 11:23:05 -0500

    Despliegue en beta url

--- out/_next/static/-EAAMlIfoi0xCN1Wuuprp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/727-363b630101039ea71078.js","static/chunks/pages/index-72424ceac1793a50a42a.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
