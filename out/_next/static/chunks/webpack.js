09b37dae944e5cf4c4208efcb16f2c1e9e657eb6
Carlos <munoz.josecarlos@gmail.com>
2021-06-14 21:37:45 -0500

    Dt for regreso

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef96f9952b257d532555"; }
+/******/ 		__webpack_require__.h = function() { return "2c130625a5b778d35faa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ out/_next/static/webpack/pages/index.1e7c9d65dd41116a570f.hot-update.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./public/regreso.json":
+/*!*****************************!*\
+  !*** ./public/regreso.json ***!
+  \*****************************/
+/***/ (function(module) {
+
+"use strict";
+module.exports = JSON.parse('{"socioemocional":[{"title":"Orientaciones Preescolar","url":"Orientaciones Preescolar 13062021_11hrs.pdf"},{"title":"Orientaciones Primaria","url":"Orientaciones Primaria 13062021_11hrs.pdf"},{"title":"Orientaciones Secundaria","url":"Orientaciones Secundaria 13062021_12hrs.pdf"}],"gpedagogicas":[{"title":"Guía Preescolar","url":"GuiaPedagogicaPreescolar_VF_05062021.pdf"},{"title":"Guía Primaria ","url":"GuiaPedagogicaPrimaria_VF_05062021.pdf"},{"title":"Guía Secundaria","url":"GuiaPedagogicaSecundaria_VF_05062021.pdf"}],"kit":[{"title":"Kit de actividades socioemocionales","url":"14_12_20_KitActividadesSocioemocionales_ConvivenciaArmonicaFamilia_210524_101518_210527_231448.pdf"}]}');
+
+/***/ })
+
+});
